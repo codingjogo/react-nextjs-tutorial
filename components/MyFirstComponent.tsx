69567fcf837502e5cd477b7e98713855ec722041
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-23 11:28:31 +0800

    feat : Add MyFirstComponent

---
+++ components/MyFirstComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// #2 Define <MyFirstComponent />
+// inside of /components/MyFirstComponent.tsx
+export const MyFirstComponent = () => {
+  return (
+    <div>
+        <h2 className='text-3xl'>My First Component!</h2>
+    </div>
+  )
+}
