6868215667c4ac630844f02411d3b67f9b8d9379
Joshua Gonzales (codingjogo) <codingjogo@gmail.com>
2024-08-23 11:23:27 +0800

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="container py-12">
+      <h1 className="mb-5 text-4xl">My Initial Commit</h1>
+      <Button size={'lg'}>Commit</Button>
+    </main>
+  );
+}
